c7f7c129c7359439f088506ea024c691308fd697
alvin.pino <alvin.pino@procuraduria.gob.pa>
2023-08-25 13:56:49 -0500

    Cambios en axios

--- src/API.js
+++ src/API.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost/backend/public/api",
-    headers:{
-        "Content-type": "application/json"
-    }
-})
+  baseURL: "http://localhost/backend/public/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
