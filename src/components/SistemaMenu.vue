7c98f0399e38d43b8c5a42d0861da1e52ee8bf50
alvin.pino <alvin.pino@procuraduria.gob.pa>
2023-08-24 22:00:45 -0500

    Nuevos cambios

--- src/components/SistemaMenu.vue
+++ src/components/SistemaMenu.vue
@@ -1,10 +1,48 @@
 <template>
     <div>
-        
+        <v-app id="inspire">
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+        >
+            <template v-slot:prepend>
+                
+            </template>
+
+        </v-navigation-drawer>
+
+        <v-app-bar app id="toolbar" dark>
+            <v-app-bar-nav-icon @click="drawer =! drawer"></v-app-bar-nav-icon>
+            <v-spacer></v-spacer>
+            <v-toolbar-title>{{ tituloToolbar }}</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-app-bar>
+
+        <v-main>
+            <router-view/>
+        </v-main>
+        </v-app>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            drawer:null,
+            titulo:-1
+        }
+    },
+
+    computed: {
+        tituloToolbar(){
+            return this.titulo === -1 ? 'Sistema de Control de Insumos' : ''
+        }
+    },
 }
 </script>
+
+<style>
+#toolbar{
+    background-color:#053565;
+}
+</style>
